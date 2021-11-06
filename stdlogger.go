849871016963e9ced747b9923d95e9ca8177fe2a
Peter Teich <peter.teich@gmail.com>
2021-11-06 16:28:09 +0100

    Compatibility funcs for stdlib

--- stdlogger.go
+++ stdlogger.go
@@ -1,17 +1,17 @@
 package logger
 
 func (log *Logger) Debugf(template string, args ...interface{}) {
-	log.Debug().Msgf(template, args)
+	log.Debug().Msgf(template, args...)
 }
 
 func (log *Logger) Infof(template string, args ...interface{}) {
-	log.Info().Msgf(template, args)
+	log.Info().Msgf(template, args...)
 }
 
 func (log *Logger) Warnf(template string, args ...interface{}) {
-	log.Warn().Msgf(template, args)
+	log.Warn().Msgf(template, args...)
 }
 
 func (log *Logger) Errorf(template string, args ...interface{}) {
-	log.Error().Msgf(template, args)
+	log.Error().Msgf(template, args...)
 }
